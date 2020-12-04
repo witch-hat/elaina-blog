7fd2fa7c0a6317adeb20b488529c51172fe683a9
ekalii <andoune0719@gmail.com>
2020-12-05 02:52:26 +0900

    create css-reset component

---
+++ client/src/components/GlobalStyles.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+    Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  line-height: 1.6;
+  font-size: 18px;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+a {
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+`;
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,1 +1,2 @@
 export { Header } from './header/Header';
+export { GlobalStyles } from './GlobalStyles';
