e58fab8ff0a170cc6239585be94f19fa36903cc9
ekalii <andoune0719@gmail.com>
2020-12-07 16:05:47 +0900

    make comments in link tags using public_url

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -12,12 +12,12 @@ export default function ElainaBlog({ Component, pageProps }: AppProps) {
       <GlobalStyles />
       <Head>
         <meta charSet='utf-8' />
-        <link rel='icon' href='%PUBLIC_URL%/favicon.ico' />
+        {/* <link rel='icon' href='%PUBLIC_URL%/favicon.ico' /> */}
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='theme-color' content='#000000' />
         <meta name='description' content='Elaina Blog Theme' />
-        <link rel='apple-touch-icon' href='%PUBLIC_URL%/logo192.png' />
-        <link rel='manifest' href='%PUBLIC_URL%/manifest.json' />
+        {/* <link rel='apple-touch-icon' href='%PUBLIC_URL%/logo192.png' /> */}
+        {/* <link rel='manifest' href='%PUBLIC_URL%/manifest.json' /> */}
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link href='https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap' rel='stylesheet' />
         <link
