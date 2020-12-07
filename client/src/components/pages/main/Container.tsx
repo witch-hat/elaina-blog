e88c9d74c4cf511fc9dd13efecaa729166748e2f
ekalii <andoune0719@gmail.com>
2020-12-07 22:42:38 +0900

    make use profile image, name

--- client/src/components/pages/main/Container.tsx
+++ client/src/components/pages/main/Container.tsx
@@ -4,14 +4,17 @@ interface Styles {
   width?: string;
   border?: string;
   padding?: string;
+  flexDirection?: 'column' | 'row';
   alignItems?: string;
+  justifyContent?: string;
 }
 
 const FlexContainer = styled.div<Styles>((props) => {
   return {
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: props.justifyContent || 'center',
     height: `calc(90vh - 40px - ${props.padding || '0px'})`,
+    flexDirection: props.flexDirection || 'row',
     alignItems: props.alignItems || 'center',
     width: props.width || '100%',
     border: props.border || 'none',
--- client/src/components/pages/main/Profile.tsx
+++ client/src/components/pages/main/Profile.tsx
@@ -1,13 +1,27 @@
 import styled from 'styled-components';
 
-import { Container } from 'components';
+import { Container, RoundImage } from 'components';
+import { color } from 'resources';
+
+const Name = styled.div({
+  fontSize: '22px',
+  fontWeight: 'bold',
+  width: '100%',
+  padding: '0 50px',
+  margin: '15px 0',
+  wordBreak: 'keep-all'
+});
 
 interface Props {}
 
 export function Profile() {
   return (
-    <Container styles={{ width: '300px', padding: '10px' }}>
-      <>This is Profile</>
+    <Container styles={{ width: '300px', padding: '10px', flexDirection: 'column', justifyContent: 'flex-start' }}>
+      <RoundImage
+        src={'/images/FakeProfile.png'}
+        styles={{ borderRadius: '50%', border: `4px solid ${color.blogName}`, width: '200px', height: '200px' }}
+      />
+      <Name>Elaina</Name>
     </Container>
   );
 }
