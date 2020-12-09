200dac217e9bc12187d53ccbe4645479fa432f8d
ekalii <andoune0719@gmail.com>
2020-12-09 20:46:01 +0900

    make Preview Contents with line clamp

--- client/src/components/pages/main/ContentCategory.tsx
+++ client/src/components/pages/main/ContentCategory.tsx
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 
 import { BorderBox } from 'components';
 import { color } from 'resources/colors';
+import { RoundImage } from 'components/common/image/RoundImage';
 
 const Title = styled.div({
   width: '100%',
-  fontSize: '22px',
+  fontSize: '1.4rem',
   fontWeight: 'bold',
   margin: '25px 0 0'
 });
@@ -15,18 +16,63 @@ const Container = styled.div({
   flexDirection: 'column',
   width: '100%',
   height: '100%',
-  padding: '.9rem',
+  padding: '.9rem 0',
   justifyContent: 'center',
-  alignItems: 'center'
+  alignItems: 'center',
+  '& > div:nth-child(1)': {
+    margin: '0 0 .8rem !important'
+  }
 });
 
 const Content = styled.div({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  width: 'calc(950px - 1.8rem)',
+  width: '950px',
   padding: '1rem',
   height: '10rem'
+});
+
+const PreviewImage = styled.img({
+  width: '300px',
+  height: '8rem',
+  objectFit: 'cover'
+});
+
+const PreviewTextWrapper = styled.div({
+  padding: '0 1rem',
+  width: 'calc(650px - 2rem)',
+  display: 'flex',
+  height: '8rem',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center'
+});
+
+const PreviewTitle = styled.div({
+  height: '2rem',
+  width: '100%',
+  fontSize: '1.4rem',
+  fontWeight: 'bold',
+  textAlign: 'left',
+  overflow: 'hidden',
+  display: '-webkit-box',
+  WebkitLineClamp: 1,
+  WebkitBoxOrient: 'vertical'
+});
+
+const PreviewContent = styled.div({
+  width: '100%',
+  height: '5rem',
+  fontSize: '1.1rem',
+  margin: '.5rem 0 0',
+  wordBreak: 'keep-all',
+  textAlign: 'left',
+  padding: '.5rem 0 0',
+  overflow: 'hidden',
+  display: '-webkit-box',
+  WebkitLineClamp: 3,
+  WebkitBoxOrient: 'vertical'
 });
 
 interface Props {}
@@ -37,10 +83,28 @@ export function ContentCategory() {
       <Title>Content Category</Title>
       <Container>
         <BorderBox styles={{ margin: '.8rem 0' }}>
-          <Content>Content1(미리보기: 사진, 제목, 내용)</Content>
+          <Content>
+            <PreviewImage src='/images/FakeProfile.png' alt='preview image' />
+            <PreviewTextWrapper>
+              <PreviewTitle>Hello!</PreviewTitle>
+              <PreviewContent>PreviewContent</PreviewContent>
+            </PreviewTextWrapper>
+          </Content>
         </BorderBox>
         <BorderBox styles={{ margin: '.8rem 0' }}>
-          <Content>Content2(미리보기: 사진, 제목, 내용)</Content>
+          <Content>
+            <PreviewImage src='/images/FakeProfile.png' alt='preview image' />
+            <PreviewTextWrapper>
+              <PreviewTitle>Very long long long long long long long long longlonglonglonglonglonglonglonglong</PreviewTitle>
+              <PreviewContent>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+                book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+                unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
+                recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </PreviewContent>
+            </PreviewTextWrapper>
+          </Content>
         </BorderBox>
         <BorderBox styles={{ margin: '.8rem 0' }}>
           <Content>Content3(미리보기: 사진, 제목, 내용)</Content>
