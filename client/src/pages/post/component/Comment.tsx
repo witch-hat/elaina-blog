b701ab773dfb6ef0cf40b4c08d1d4b14712e3789
ekalii <andoune0719@gmail.com>
2020-12-11 18:51:48 +0900

    add marginTop

--- client/src/pages/post/component/Comment.tsx
+++ client/src/pages/post/component/Comment.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 const Container = styled.div({
   width: '800px',
   display: 'flex',
+  marginTop: '20px',
   flexDirection: 'column',
   jusitfyContent: 'flex-start',
   alignItems: 'center',
