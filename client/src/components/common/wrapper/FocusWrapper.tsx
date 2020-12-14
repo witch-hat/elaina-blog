ef2db8039d0551440df5e775999793066143b0b2
An DoEon <andoune0719@gmail.com>
2020-12-14 15:09:31 +0900

    add FocusWrapper

---
+++ client/src/components/common/wrapper/FocusWrapper.tsx
@@ -0,0 +1,36 @@
+import React, { useImperativeHandle, useRef, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div({
+  position: 'relative'
+});
+
+interface Props {
+  children: JSX.Element;
+  visible: boolean;
+  setVisible: Function;
+}
+
+export const FocusWrapper = React.forwardRef<HTMLDivElement, Props>((props: Props, forwardedRef) => {
+  const focusRef = useRef<HTMLDivElement>(null);
+  useImperativeHandle(forwardedRef, () => focusRef.current as HTMLDivElement);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (props.visible && !focusRef.current?.contains(event.target as Node)) {
+        props.setVisible(false);
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [props]);
+
+  if (props.visible) {
+    return <Wrapper ref={focusRef}>{props.children}</Wrapper>;
+  } else {
+    return null;
+  }
+});
