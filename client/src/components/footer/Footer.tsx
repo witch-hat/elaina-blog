0c80cadffb2fa9b7769dd016e3f2f08adf2cb53a
An DoEon <andoune0719@gmail.com>
2020-12-14 17:44:49 +0900

    add border, fix width

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer({
-  width: '100vw',
+  width: '100%',
   height: '3rem',
   display: 'flex',
   justifyContent: 'center',
-  alignItems: 'center'
+  alignItems: 'center',
+  borderTop: '1px solid #ddd'
 });
 
 export function Footer() {
