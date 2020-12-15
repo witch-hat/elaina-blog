b4bf0666ea71fe96e560f40c1e10912384cd8c96
An DoEon <andoune0719@gmail.com>
2020-12-15 22:21:21 +0900

    Header Responsive Design: but cannot use animation...

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import { ThemeMode } from 'src/redux/common/type';
 import { theme } from 'src/resources';
@@ -31,7 +31,10 @@ const Container = styled.div({
   padding: '0 4rem',
   margin: '0 auto',
   alignItems: 'center',
-  justifyContent: 'space-between'
+  justifyContent: 'space-between',
+  '@media screen and (max-width: 768px)': {
+    padding: '0 1rem'
+  }
 });
 
 const BlogName = styled.div({
@@ -74,12 +77,71 @@ const Flex = styled.div({
   justifyContent: 'center'
 });
 
+const MobileMenuButton = styled.div({
+  display: 'none',
+  padding: '.5rem',
+  fontSize: '1.5rem',
+  '@media screen and (max-width: 768px)': {
+    display: 'block'
+  }
+});
+
+const OpeningAnimation = keyframes`
+  0% {
+    background-color: red;
+  }
+  100% {
+    background-color: blue;
+  }
+`;
+
+const ResponsiveMenuBox = styled.div<{ isOpen?: boolean }>((props) => {
+  return props.isOpen
+    ? {
+        display: 'flex',
+        '@media screen and (max-width: 768px)': {
+          padding: '10px',
+          position: 'fixed',
+          top: '5rem',
+          right: '0',
+          zIndex: 9999,
+          backgroundColor: '#eee',
+          borderRadius: '12px',
+          boxShadow: '0 8px 4px -4px rgba(38, 38, 38, .4)',
+          animation: `1s ${OpeningAnimation.getName()} .1s ease-out infinite`
+        }
+      }
+    : {
+        display: 'flex',
+        '@media screen and (max-width: 768px)': {
+          display: 'none'
+        }
+      };
+});
+
 interface Props {
   name: string;
   theme: ThemeMode;
 }
 
 export function Header(props: Props) {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return window.removeEventListener('resize', handleResize);
+  }, []);
+
+  function onMobileMenuButtonClick() {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  }
+
   return (
     <StyledHeader themeMode={props.theme}>
       <Container>
@@ -87,14 +149,19 @@ export function Header(props: Props) {
           <BlogName>{props.name}</BlogName>
         </Link>
         <Flex>
-          <ModeSwitch />
-          <SearchForm method='GET' action='/search'>
-            <InputBox type='text' placeholder='Search' id='search' minLength={2} maxLength={10} styles={{ width: '180px' }} />
-            <SearchButton type='submit'>
-              <i className='fas fa-search'></i>
-            </SearchButton>
-          </SearchForm>
+          <ResponsiveMenuBox isOpen={isMobileMenuOpen && windowWidth <= 768}>
+            <ModeSwitch />
+            <SearchForm method='GET' action='/search'>
+              <InputBox type='text' placeholder='Search' id='search' minLength={2} maxLength={10} styles={{ width: '180px' }} />
+              <SearchButton type='submit'>
+                <i className='fas fa-search'></i>
+              </SearchButton>
+            </SearchForm>
+          </ResponsiveMenuBox>
           <AdminMenuButton />
+          <MobileMenuButton onClick={() => onMobileMenuButtonClick()}>
+            <i className='fas fa-bars'></i>
+          </MobileMenuButton>
         </Flex>
       </Container>
     </StyledHeader>
