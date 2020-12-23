66da1ea246b9607dd9170ec7601b14c16257ce1b
An DoEon <andoune0719@gmail.com>
2020-12-23 15:19:31 +0900

    make mock-up data and type

--- client/src/resources/index.ts
+++ client/src/resources/index.ts
@@ -1,1 +1,43 @@
-export {};
+export interface CommentData {
+  name: string;
+  time: string;
+  comment: any;
+  reply: Array<{ name: string; time: string; comment: any }>;
+}
+
+export interface PostData {
+  url: string;
+  title: string;
+  author: string;
+  time: string;
+  article: any;
+  comments: Array<CommentData>;
+}
+
+export interface CategoryData {
+  url: string;
+  name: string;
+  description: string;
+  previewImage: string;
+  posts: Array<PostData>;
+}
+
+export interface Data {
+  blogName: string;
+  admin: {
+    id: string;
+    password: string;
+  };
+  profile: {
+    name: string;
+    introduce: string;
+    link: string;
+    company: string;
+    email: string;
+  };
+  category: Array<CategoryData>;
+}
+
+const mockUpData: Data = require('./mockup.json');
+
+export { mockUpData };
