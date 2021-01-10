ee7dd22e2463add079dd472455a11045c87cb6ed
An DoEon <andoune0719@gmail.com>
2021-01-10 20:40:07 +0900

    add lists

--- client/src/components/header/AdminMenuButton.tsx
+++ client/src/components/header/AdminMenuButton.tsx
@@ -3,10 +3,15 @@ import styled from 'styled-components';
 import Link from 'next/link';
 
 import { theme } from 'src/styles';
+import { FocusWrapper } from 'src/components';
 
 import { useSelector } from 'react-redux';
 import { RootState } from 'src/redux/rootReducer';
 import { ThemeMode } from 'src/redux/common/type';
+
+const Container = styled.div({
+  position: 'relative'
+});
 
 const Button = styled.div<{ themeMode: string }>((props) => ({
   flexShrink: 0,
@@ -20,6 +25,36 @@ const Button = styled.div<{ themeMode: string }>((props) => ({
   }
 }));
 
+const RotateIcon = styled.i<{ isOpen: boolean }>((props) => {
+  return {
+    display: 'inline-block',
+    transition: '.3s all',
+    transform: props.isOpen ? 'rotate(180deg)' : 'none'
+  };
+});
+
+const ListContainer = styled.div<{ themeMode: ThemeMode }>((props) => ({
+  position: 'absolute',
+  right: '10px',
+  top: '.5rem',
+  display: 'flex',
+  width: '10rem',
+  flexDirection: 'column',
+  borderRadius: '12px',
+  backgroundColor: theme[props.themeMode].secondaryContentBackground,
+  zIndex: 1
+}));
+
+const List = styled.div({
+  padding: '.75rem',
+  textAlign: 'center',
+  borderRadius: '12px',
+  cursor: 'pointer',
+  '&:hover': {
+    backgroundColor: '#888'
+  }
+});
+
 interface Props {}
 
 export default function AdminMenuButton(props: Props) {
@@ -27,10 +62,32 @@ export default function AdminMenuButton(props: Props) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <Link href='/admin'>
+    <Container>
       <Button onClick={() => setIsMenuOpen(!isMenuOpen)} themeMode={themeMode}>
         Menu&nbsp;
+        <RotateIcon className='fas fa-caret-down' isOpen={isMenuOpen} />
       </Button>
-    </Link>
+      {isMenuOpen && (
+        <FocusWrapper visible={isMenuOpen} onClickOutside={() => setIsMenuOpen(false)}>
+          <ListContainer themeMode={themeMode}>
+            <Link href='/admin'>
+              <List>Admin</List>
+            </Link>
+            <Link href='/admin'>
+              <List>Admin</List>
+            </Link>
+            <Link href='/admin'>
+              <List>Admin</List>
+            </Link>
+            <Link href='/admin'>
+              <List>Admin</List>
+            </Link>
+            <Link href='/admin'>
+              <List>Admin</List>
+            </Link>
+          </ListContainer>
+        </FocusWrapper>
+      )}
+    </Container>
   );
 }
