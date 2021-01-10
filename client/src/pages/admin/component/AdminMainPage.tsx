a67dc55445f6b54b6a79b92c3eac65a918fa1ed2
An DoEon <andoune0719@gmail.com>
2021-01-10 22:17:13 +0900

    add admin page sidebar

--- client/src/pages/admin/component/AdminMainPage.tsx
+++ client/src/pages/admin/component/AdminMainPage.tsx
@@ -3,12 +3,35 @@ import styled from 'styled-components';
 
 import { ContentCategoryModifier } from './ContentCategoryModifier';
 import { ViewCountAnalyst } from './ViewCountAnalyst';
+import { SideBar } from './SideBar';
+
+const Container = styled.div({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+});
+
+const Side = styled.aside({
+  width: '300px',
+  height: '100%'
+});
+
+const Section = styled.section({
+  width: '850px',
+  marginLeft: '50px'
+});
 
 export function AdminMainPage() {
   return (
-    <div>
-      <ViewCountAnalyst />
-      <ContentCategoryModifier />
-    </div>
+    <Container>
+      <Side>
+        <SideBar />
+      </Side>
+      <Section>
+        <ViewCountAnalyst />
+        <ContentCategoryModifier />
+      </Section>
+    </Container>
   );
 }
---
+++ client/src/pages/admin/component/SideBar.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from 'styled-components';
+import Link from 'next/link';
+
+import Profile from 'src/components/main/Profile';
+
+interface Props {}
+
+const Container = styled.div({
+  width: '100%'
+});
+
+const TitleWrapper = styled.div({
+  display: 'flex',
+  alignItems: 'center',
+  marginBottom: '.5rem'
+});
+
+const Icon = styled.span({
+  display: 'inline-flex',
+  width: '1.5rem',
+  height: '1.5rem',
+  justifyContent: 'flex-start',
+  alignItems: 'center'
+});
+
+const Title = styled.p({
+  width: '100%',
+  fontSize: '1.25rem',
+  fontWeight: 'bold'
+});
+
+const ListContainer = styled.ul({
+  width: '100%',
+  marginTop: '0'
+});
+
+const List = styled.li({
+  width: '100%',
+  listStyle: 'none',
+  paddingLeft: '1rem',
+  cursor: 'pointer',
+  margin: '.125rem 0',
+  '&:hover': {
+    textDecoration: 'underline'
+  }
+});
+
+const Button = styled.button({
+  width: '100%',
+  padding: '.625rem',
+  borderRadius: '1rem',
+  marginBottom: '1rem'
+});
+
+export function SideBar() {
+  return (
+    <Container>
+      <Link href='admin/writer'>
+        <Button>글쓰기</Button>
+      </Link>
+      <TitleWrapper>
+        <Icon>
+          <i className='fas fa-pager'></i>
+        </Icon>
+        <Title>컨텐츠</Title>
+      </TitleWrapper>
+      <ListContainer>
+        <List>카테고리 관리</List>
+        <List>글 관리</List>
+        <List>댓글 관리</List>
+      </ListContainer>
+      <TitleWrapper>
+        <Icon>
+          <i className='fas fa-chart-line'></i>
+        </Icon>
+        <Title>통계</Title>
+      </TitleWrapper>
+      <ListContainer>
+        <List>조회수</List>
+      </ListContainer>
+    </Container>
+  );
+}
