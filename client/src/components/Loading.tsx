a6076ddb02ba4b524061102ee91b91123879d40a
An DoEon <andoune0719@gmail.com>
2021-01-12 15:27:28 +0900

    add animation

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 const Container = styled.div({
   width: '100%',
@@ -9,9 +9,30 @@ const Container = styled.div({
   alignItems: 'center'
 });
 
+const RotateAnimation = keyframes({
+  from: {
+    transform: 'rotate(0deg)'
+  },
+  to: {
+    transform: 'rotate(360deg)'
+  }
+});
+
+const RotatingIcon = styled.span(
+  {
+    marginRight: '.25rem'
+  },
+  css`
+    animation: ${RotateAnimation} 0.75s infinite;
+  `
+);
+
 export function Loading() {
   return (
     <Container>
+      <RotatingIcon>
+        <i className='fas fa-spinner'></i>
+      </RotatingIcon>
       <p>Loading</p>
     </Container>
   );
