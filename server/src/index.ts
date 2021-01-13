1a663394660782130e16d3284131f3129bcb4c82
An DoEon <andoune0719@gmail.com>
2021-01-13 16:18:28 +0900

    connect to mongodb atlas db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,8 @@
 import { gql, ApolloServer } from 'apollo-server';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const typeDefs = gql`
   type Book {
@@ -31,5 +35,21 @@ const resolvers = {
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-  console.log(`Server Ready! ${url}`);
+  console.log(`Server Ready at ${url}`);
 });
+
+console.log(process.env.DB_USER);
+
+const MONGO_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@elainatest.c88ha.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
