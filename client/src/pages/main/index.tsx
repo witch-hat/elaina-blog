1c4bdaa134a984524e79add8934b28a6ced5c40c
An DoEon <andoune0719@gmail.com>
2021-01-25 00:08:50 +0900

    delete test code

--- client/src/pages/main/index.tsx
+++ client/src/pages/main/index.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
 
-import { Loading } from 'src/components';
-import { ContentCategory } from 'src/pages/main/component';
+import { ContentCategory } from 'src/pages/main/component/ContentCategory';
 import { MainPageLayout } from 'src/components';
 
-const GET_NAME = gql`
-  query profile {
-    profile {
-      name
-    }
-  }
-`;
-
 export default function Main() {
-  const { loading, error, data } = useQuery(GET_NAME);
-
-  if (loading) return <Loading />;
-  console.log('test', data.profile[0].name);
-
   return (
     <MainPageLayout>
       <ContentCategory />
