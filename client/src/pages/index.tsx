9d292261881dd245801c2988a01b40443edd6b11
An DoEon <andoune0719@gmail.com>
2021-02-09 23:31:33 +0900

    move getStaticProps: pages/main/index.tsx to pages/index.tsx

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { GET_PROFILE, ProfileType, UPDATE_PROFILE } from 'src/query';
+import { NextPageContext, InferGetStaticPropsType } from 'next';
+import { initializeApollo } from 'src/apollo/apolloClient';
+
 import Main from './main';
 
-export default function Index() {
-  return <Main></Main>;
+export default function Index({ profile }: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <Main profile={profile}></Main>;
+}
+
+export async function getStaticProps(context: NextPageContext) {
+  const apolloClient = initializeApollo(null);
+  const { data } = await apolloClient.query({ query: GET_PROFILE });
+  const profile = data.profile;
+
+  return {
+    props: {
+      profile
+    }
+  };
 }
--- client/src/pages/main/index.tsx
+++ client/src/pages/main/index.tsx
@@ -1,27 +1,16 @@
 import React from 'react';
-import { NextPageContext, InferGetStaticPropsType } from 'next';
 
 import { ContentCategory } from 'src/pages/main/component/ContentCategory';
 import { MainPageLayout, Loading } from 'src/components';
-import { GET_PROFILE, ProfileType, UPDATE_PROFILE } from 'src/query';
-import { initializeApollo } from 'src/apollo/apolloClient';
 
-export default function Main({ profile }: InferGetStaticPropsType<typeof getStaticProps>) {
+interface Props {
+  profile: never;
+}
+
+export default function Main({ profile }: Props) {
   return (
     <MainPageLayout profile={profile}>
       <ContentCategory />
     </MainPageLayout>
   );
-}
-
-export async function getStaticProps(context: NextPageContext) {
-  const apolloClient = initializeApollo(null);
-  const { data } = await apolloClient.query({ query: GET_PROFILE });
-  const profile = data.profile;
-
-  return {
-    props: {
-      profile
-    }
-  };
 }
