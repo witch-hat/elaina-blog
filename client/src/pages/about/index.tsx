33c3eaa1062fb65da576057852d2b6fd94d93682
An DoEon <andoune0719@gmail.com>
2021-02-09 23:59:11 +0900

    add getStaticProps for get Profile data

--- client/src/pages/about/index.tsx
+++ client/src/pages/about/index.tsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
+import { NextPageContext, InferGetStaticPropsType } from 'next';
 
+import { GET_PROFILE } from 'src/query';
+import { initializeApollo } from 'src/apollo/apolloClient';
 import { MainPageLayout } from 'src/components';
 
 import { useSelector } from 'react-redux';
@@ -8,12 +11,24 @@ import { ThemeMode } from 'src/redux/common/type';
 
 const Container = styled.section({});
 
-export default function About() {
+export default function About({ profile }: InferGetStaticPropsType<typeof getStaticProps>) {
   const theme: ThemeMode = useSelector<RootState, any>((state) => state.common.theme);
 
   return (
-    <MainPageLayout>
+    <MainPageLayout profile={profile}>
       <div>About</div>
     </MainPageLayout>
   );
+}
+
+export async function getStaticProps(context: NextPageContext) {
+  const apolloClient = initializeApollo(null);
+  const { data } = await apolloClient.query({ query: GET_PROFILE });
+  const profile = data.profile;
+
+  return {
+    props: {
+      profile
+    }
+  };
 }
--- client/src/pages/timeline/index.tsx
+++ client/src/pages/timeline/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import { NextPageContext, InferGetStaticPropsType } from 'next';
 
+import { GET_PROFILE } from 'src/query';
+import { initializeApollo } from 'src/apollo/apolloClient';
 import { MainPageLayout } from 'src/components';
 import { TimeLineEditor } from './component/TimeLineEditor';
 
@@ -8,12 +11,24 @@ const Container = styled.div({
   margin: '2rem 0 0'
 });
 
-export default function TimeLine() {
+export default function TimeLine({ profile }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <MainPageLayout>
+    <MainPageLayout profile={profile}>
       <Container>
         <TimeLineEditor />
       </Container>
     </MainPageLayout>
   );
+}
+
+export async function getStaticProps(context: NextPageContext) {
+  const apolloClient = initializeApollo(null);
+  const { data } = await apolloClient.query({ query: GET_PROFILE });
+  const profile = data.profile;
+
+  return {
+    props: {
+      profile
+    }
+  };
 }
