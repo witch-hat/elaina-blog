7aa30c473077840f28109dfb000f615eccde2e95
An DoEon <andoune0719@gmail.com>
2021-02-13 01:23:32 +0900

    delete fontawesome cdn link

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -38,12 +38,6 @@ export default function ElainaBlog({ Component, pageProps }: AppProps) {
             {/* <link rel='manifest' href='%PUBLIC_URL%/manifest.json' /> */}
             <link rel='preconnect' href='https://fonts.gstatic.com' />
             <link href='https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap' rel='stylesheet' />
-            <link
-              rel='stylesheet'
-              href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css'
-              integrity='sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=='
-              crossOrigin='anonymous'
-            />
             <title>Elaina Blog</title>
           </Head>
           <Layout {...pageProps}>
