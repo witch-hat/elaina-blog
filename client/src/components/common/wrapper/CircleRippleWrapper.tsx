2d3c428c79139fab3b1920b959e35adf44aa6125
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2021-03-13 23:05:58 +0900

    Get Categories

---
+++ client/src/components/common/wrapper/CircleRippleWrapper.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import styled, { keyframes, css } from 'styled-components';
+
+const Wrapper = styled.div({
+  display: 'flex',
+  alignContent: 'center',
+  justifyContent: 'center',
+  padding: '8px'
+});
+
+const rippleAnimation = keyframes({
+  from: {
+    opacity: 0.3,
+    transform: 'scale(0.3)'
+  },
+  to: {
+    opacity: 1,
+    transform: 'scale(1)'
+  }
+});
+
+const Ripple = styled.div(
+  (props) => ({
+    display: 'block',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    borderRadius: '50%',
+    top: '0',
+    left: '0',
+    right: '0',
+    bottom: '0',
+    backgroundColor: 'rgba(37, 37, 37, 0.1)'
+  }),
+  css`
+    & {
+      animation: ${rippleAnimation} 150ms cubic-bezier(0.4, 0.2, 0.68, 0.53);
+    }
+  `
+);
+
+interface Props {
+  children: JSX.Element;
+  onClick: () => void;
+}
+
+export function CircleRippleWrapper(props: Props) {
+  const [isClick, setIsClick] = useState<boolean>(false);
+
+  return (
+    <Wrapper onClick={props.onClick}>
+      {/* <div style={{ display: 'block' }}>
+        <Ripple />
+      </div> */}
+      {props.children}
+      {/* <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'rgb(12,32,42)'
+        }}
+      />
+      <div
+        style={{
+          width: '200px',
+          height: '200px',
+          position: 'relative',
+          backgroundColor: 'rgb(132,132,212)'
+        }}
+      /> */}
+    </Wrapper>
+  );
+}
--- client/src/pages/admin/category/index.tsx
+++ client/src/pages/admin/category/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGripVertical, faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { NextPageContext } from 'next';
 
 import { BorderBox } from 'src/components';
 import { mockUpData } from 'src/resources';
 import { AdminPageLayout } from '../component/AdminPageLayout';
 import { theme } from 'src/styles';
-
-import { useSelector } from 'react-redux';
+import { GET_CATEGORIES_WITH_DETAILS, CategoryDetails } from 'src/query/category';
 import { RootState } from 'src/redux/rootReducer';
 import { ThemeMode } from 'src/redux/common/type';
+import { CircleRippleWrapper } from 'src/components/common/wrapper/CircleRippleWrapper';
+import { initApolloClient } from 'src/apollo/withApollo';
+import { AppCommonProps } from 'src/pages/_app';
 
 const Container = styled.div({
   width: '100%',
@@ -102,34 +108,148 @@ const Button = styled.button<{ danger?: boolean; themeMode: ThemeMode }>((props)
   }
 }));
 
-export default function Category() {
+const InputContainer = styled.div({
+  display: 'flex',
+  width: '100%',
+  alignItems: 'center',
+  margin: '.71rem 0'
+});
+
+const Input = styled.input<{ themeMode: ThemeMode }>((props) => ({
+  display: 'inline-block',
+  width: '100%',
+  height: '2rem',
+  fontSize: '1.1rem',
+  padding: '.2rem',
+  outline: 'none',
+  fontWeight: 'normal',
+  border: `1px solid ${theme[props.themeMode].inputBorder}`,
+  borderRadius: '8px',
+  color: theme[props.themeMode].inputText,
+  backgroundColor: theme[props.themeMode].inputBackground
+}));
+
+interface Props extends AppCommonProps {
+  categories: CategoryDetails[];
+}
+
+export default function Category(props: Props) {
+  const [editingCategoryId, setEditingCategoryId] = useState<number>(-1);
   const themeMode: ThemeMode = useSelector<RootState, any>((state) => state.common.theme);
+  console.log(props.categories);
+
+  function deleteCategory() {}
+
+  function editCategory(index: number) {
+    if (editingCategoryId === index) {
+      setEditingCategoryId(-1);
+    } else {
+      setEditingCategoryId(index);
+    }
+  }
+
+  function saveEditing(index: number) {}
+
+  function checkEditing() {}
 
   return (
     <AdminPageLayout>
       <Container>
-        {mockUpData.category.map((category) => {
-          return (
-            <CategoryContainer>
-              <BorderBox isTransform={false} styles={{ width: '100%', margin: '.8rem 0' }}>
-                <Content>
-                  <PreviewTextWrapper>
-                    <PreviewTitle>{category.name}</PreviewTitle>
-                    <PreviewContent>{category.description}</PreviewContent>
-                  </PreviewTextWrapper>
-                  <PreviewImage src='/images/FakeProfile.png' alt='preview image' />
-                </Content>
-              </BorderBox>
-              <ButtonContainer>
-                <Button themeMode={themeMode}>Edit</Button>
-                <Button themeMode={themeMode} danger>
-                  Delete
-                </Button>
-              </ButtonContainer>
-            </CategoryContainer>
-          );
+        {props.categories.map((category, index) => {
+          if (index === editingCategoryId) {
+            return (
+              <CategoryContainer key={category.title}>
+                <BorderBox isTransform={false} styles={{ width: '100%', margin: '.8rem 0' }}>
+                  <Content>
+                    <PreviewTextWrapper>
+                      <Input type={'text'} themeMode={themeMode} defaultValue={category.title} />
+                      <Input type={'text'} themeMode={themeMode} defaultValue={category.description} />
+                    </PreviewTextWrapper>
+                    <PreviewImage src={category.previewImage} alt='preview image' />
+                  </Content>
+                </BorderBox>
+                <ButtonContainer>
+                  <Button
+                    themeMode={themeMode}
+                    onClick={() => {
+                      editCategory(index);
+                    }}
+                  >
+                    Edit
+                  </Button>
+                  <Button themeMode={themeMode} danger>
+                    Delete
+                  </Button>
+                </ButtonContainer>
+              </CategoryContainer>
+            );
+          } else {
+            return (
+              <CategoryContainer key={category.title}>
+                <BorderBox isTransform={false} styles={{ width: '100%', margin: '.8rem 0' }}>
+                  <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+                    <div style={{ flex: 1, display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', padding: '4px 8px 0px 8px' }}>
+                      <CircleRippleWrapper
+                        onClick={() => {
+                          console.log('HERE');
+                        }}
+                      >
+                        <FontAwesomeIcon icon={faPen} style={{ fontSize: '1.25rem' }} />
+                      </CircleRippleWrapper>
+                      <CircleRippleWrapper
+                        onClick={() => {
+                          console.log('HERE');
+                        }}
+                      >
+                        <FontAwesomeIcon icon={faTrash} style={{ fontSize: '1.25rem' }} />
+                      </CircleRippleWrapper>
+                      <CircleRippleWrapper
+                        onClick={() => {
+                          console.log('HERE');
+                        }}
+                      >
+                        <FontAwesomeIcon icon={faGripVertical} style={{ fontSize: '1.25rem' }} />
+                      </CircleRippleWrapper>
+                    </div>
+                    <Content>
+                      <PreviewTextWrapper>
+                        <PreviewTitle>{category.title}</PreviewTitle>
+                        <PreviewContent>{category.description}</PreviewContent>
+                      </PreviewTextWrapper>
+                      <PreviewImage src={category.previewImage} alt='preview image' />
+                    </Content>
+                  </div>
+                </BorderBox>
+                <ButtonContainer>
+                  <Button
+                    themeMode={themeMode}
+                    onClick={() => {
+                      editCategory(index);
+                    }}
+                  >
+                    Edit
+                  </Button>
+                  <Button themeMode={themeMode} danger>
+                    Delete
+                  </Button>
+                </ButtonContainer>
+              </CategoryContainer>
+            );
+          }
         })}
       </Container>
     </AdminPageLayout>
   );
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const apolloClient = initApolloClient({}, context);
+  const categoryQueryResult = await apolloClient.query({ query: GET_CATEGORIES_WITH_DETAILS });
+
+  const categories: CategoryDetails[] = categoryQueryResult.data.categoriesWithDetails;
+  return {
+    props: {
+      categories
+    }
+  };
 }
