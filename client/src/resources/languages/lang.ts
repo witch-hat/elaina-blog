78aeb2a9742d07d21e5368e2b5c40babeb2881ff
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2021-03-29 00:45:19 +0900

    Add Langauge Data

--- client/src/resources/languages/lang.ts
+++ client/src/resources/languages/lang.ts
@@ -4,5 +4,20 @@ export enum LangCode {
 }
 
 export enum Lang {
-  Board = 'Board'
+  Board = 'Board',
+  TimeLine = 'TimeLine',
+  About = 'About',
+  Search = 'Search',
+  Menu = 'Menu',
+  Admin = 'Admin',
+  Login = 'Login',
+  Logout = 'Logout',
+  Content = 'Content',
+  CategoryManage = 'CategoryManage',
+  BoardManage = 'BoardManage',
+  CommentManage = 'CommentManage',
+  Statistic = 'Statistic',
+  VisitorViews = 'VisitorViews',
+  Setting = 'Setting',
+  NameChange = 'NameChange'
 }
