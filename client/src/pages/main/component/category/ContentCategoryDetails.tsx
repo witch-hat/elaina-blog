f48d2b89c80e3fd84dfa8f55fcb00faa1992753b
An DoEon <andoune0719@gmail.com>
2021-05-01 03:12:31 +0900

    UI Refactoring: use theme

--- client/src/pages/main/component/category/ContentCategoryDetails.tsx
+++ client/src/pages/main/component/category/ContentCategoryDetails.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock, faBook } from '@fortawesome/free-solid-svg-icons';
 
 import { FormatUnifier } from 'src/utils';
+import { theme } from 'src/styles';
+import { RootState } from 'src/redux/rootReducer';
+import { ThemeMode } from 'src/redux/common/type';
 
-const Container = styled.div({
+const Container = styled.div<{ themeMode: ThemeMode }>((props) => ({
   marginTop: '.4rem',
-  color: '#666',
+  color: theme[props.themeMode].detailText,
   fontSize: '.8rem',
   display: 'flex'
-});
+}));
 
 const LatestTime = styled.span({
   display: 'flex',
@@ -29,8 +33,10 @@ interface Props {
 }
 
 export function ContentCategoryDetails(props: Props) {
+  const themeMode: ThemeMode = useSelector<RootState, any>((state) => state.common.theme);
+
   return (
-    <Container>
+    <Container themeMode={themeMode}>
       <LatestTime>
         <FontAwesomeIcon icon={faClock} style={{ marginRight: '5px' }} />
         <p>{props.time !== null ? FormatUnifier.getFullFormatDate(new Date(props.time)) : 'None'}</p>
