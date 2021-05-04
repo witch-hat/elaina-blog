7c456b8a3b64bacfdcf9a13a9c6301322c95dc72
S3-20181650 <39684697+S3-20181650@users.noreply.github.com>
2021-05-04 18:20:12 +0900

    commit

--- client/src/pages/admin/index.tsx
+++ client/src/pages/admin/index.tsx
@@ -1,15 +1,100 @@
-import React from 'react';
-import { NextPageContext } from 'next';
+import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
+import { NextPageContext } from "next";
 
-import { AdminPageLayout } from './component/AdminPageLayout';
-import { AppCommonProps, appCommponProps } from '../_app';
+import { AdminPageLayout } from "./component/AdminPageLayout";
+import { AppCommonProps, appCommponProps } from "../_app";
 
 interface Props extends AppCommonProps {}
+
+const Container = styled.div({
+  display: "flex",
+  width: "100%",
+  height: "100%",
+  padding: ".9rem 0",
+  flexDirection: "column",
+  "& > div:nth-child(1)": {
+    margin: "0 0 .8rem !important",
+  },
+});
+
+const Context = styled.div({
+  display: "flex",
+  width: "700px",
+  height: "7rem",
+  padding: ".8rem",
+  justifyContent: "left",
+  border: "2px solid #666",
+  borderRadius: "12px",
+  float: "left",
+  marginTop: "5px",
+  marginBottom: "20px",
+  flexDirection: "column",
+});
+
+const UserImage = styled.img({
+  display: "block",
+  float: "left",
+  width: "40px",
+  height: "40px",
+  objectFit: "cover",
+  marginRight: "5px",
+  "@media screen and (max-width: 1380px)": {
+    width: "40px",
+  },
+});
 
 export default function Admin(props: Props) {
   return (
     <AdminPageLayout>
-      <div>Admin Page</div>
+      <Container>
+        <div>Admin log</div>
+        <div>
+          <UserImage src="/public/images/FakeProfile.png"></UserImage>
+          <div>user Upload 1 Month ago</div>
+          <Link href="./post/1">
+            <Context>
+              <p>Styled component</p>
+              Preview context
+              <h6>time</h6>
+            </Context>
+          </Link>
+        </div>
+        <div>
+          <UserImage src="/public/images/FakeProfile.png"></UserImage>
+          <div>user Upload 1 Month ago</div>
+          <Link href="./post/1">
+            <Context>
+              <p>Styled component</p>
+              Comment
+              <h6>time</h6>
+            </Context>
+          </Link>
+        </div>
+        <div>
+          <UserImage src="/public/images/FakeProfile.png"></UserImage>
+          <div>user Upload 1 Month ago</div>
+          <Link href="./post/1">
+            <Context>
+              <p>Styled component</p>
+              New Category
+              <h6>time</h6>
+            </Context>
+          </Link>
+        </div>
+        <div>
+          <UserImage src="/public/images/FakeProfile.png"></UserImage>
+          <div>user Upload 1 Month ago</div>
+          <Link href="./post/1">
+            <Context>
+              <p>Styled component</p>
+              Delete Category
+              <h6>time</h6>
+            </Context>
+          </Link>
+        </div>
+      </Container>
     </AdminPageLayout>
   );
 }
@@ -19,12 +104,12 @@ export async function getServerSideProps(context: NextPageContext) {
     return {
       redirect: {
         permanent: false,
-        destination: '/admin/login'
-      }
+        destination: "/admin/login",
+      },
     };
   }
 
   return {
-    props: {}
+    props: {},
   };
 }
