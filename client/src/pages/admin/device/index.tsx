9a5320b72276f4c72fcb680b97e7aefd2e78712e
An DoEon <andoune0719@gmail.com>
2021-06-29 11:41:25 +0900

    Init device management page

--- client/src/pages/admin/device/index.tsx
+++ client/src/pages/admin/device/index.tsx
@@ -2,32 +2,68 @@ import React from 'react';
 import styled from 'styled-components';
 import { GetServerSideProps } from 'next';
 
+import { initApolloClient } from 'src/apollo/withApollo';
+import { appCommponProps } from 'src/pages/_app';
 import { trans, Lang } from 'src/resources/languages';
 
 import { AdminPageLayout } from '../component/AdminPageLayout';
+import { GET_DEVICES, LoginDevice } from 'src/query/user';
+
 import { PageTitle } from '../component/PageTitle';
+import { DeviceItem } from './component/DeviceItem';
 
 const Container = styled.div({
   width: '100%'
 });
 
-interface ServerSideProps {}
+const DeviceContainer = styled.div({
+  display: 'flex',
+  width: '100%',
+  flexWrap: 'wrap',
+  alignItems: 'center',
+  justifyContent: 'flex-start'
+});
+
+interface ServerSideProps {
+  loginDevices: LoginDevice[];
+}
 
 interface Props extends ServerSideProps {}
 
 export default function DevicePage(props: Props) {
+  console.log(props.loginDevices);
+
   return (
     <AdminPageLayout>
       <Container>
         <PageTitle title={trans(Lang.DeviceManage)} />
-        <div>Login Device Management</div>
+        <DeviceContainer>
+          {props.loginDevices.map((device) => {
+            return <DeviceItem device={device} />;
+          })}
+        </DeviceContainer>
       </Container>
     </AdminPageLayout>
   );
 }
 
 export const getServerSideProps: GetServerSideProps<ServerSideProps> = async (context) => {
+  if (!appCommponProps.app.isLogin) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/admin/login?url=%2Fadmin%2Fdevice'
+      }
+    };
+  }
+
+  const client = initApolloClient({}, context);
+  const { data } = await client.query({ query: GET_DEVICES });
+  const loginDevices: LoginDevice[] = data.findDevices;
+
   return {
-    props: {}
+    props: {
+      loginDevices
+    }
   };
 };
