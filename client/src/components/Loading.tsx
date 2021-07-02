b805fdbf94cd7aee186505860d9f10bdb5a8979e
An DoEon <andoune0719@gmail.com>
2021-07-03 01:28:02 +0900

    delete themeMode props

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -6,7 +6,7 @@ import { theme } from 'src/styles';
 import { RootState } from 'src/redux/rootReducer';
 import { ThemeMode } from 'src/redux/common/type';
 
-const Container = styled.div<{ themeMode: ThemeMode }>((props) => ({
+const Container = styled.div((props) => ({
   display: 'flex',
   position: 'fixed',
   top: '0',
@@ -42,10 +42,10 @@ const RotatingIcon = styled.span(
 );
 
 export function Loading() {
-  const themeMode: ThemeMode = useSelector<RootState, any>((state) => state.common.theme);
+  // const themeMode: ThemeMode = useSelector<RootState, any>((state) => state.common.theme);
 
   return (
-    <Container themeMode={themeMode}>
+    <Container>
       <RotatingIcon>
         <i className='fas fa-spinner'></i>
       </RotatingIcon>
