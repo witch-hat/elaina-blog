a693e0c51031a3c9780a4e271b7458d885794c3e
An DoEon <andoune0719@gmail.com>
2021-07-19 22:08:23 +0900

    UI Refactoring: centering post

--- client/src/pages/post/[post-id]/index.tsx
+++ client/src/pages/post/[post-id]/index.tsx
@@ -13,18 +13,12 @@ import { AppCommonProps } from 'src/pages/_app';
 
 import { ArticleContainer } from './component/article/ArticleContainer';
 import { CommentContainer } from './component/comment/CommentContainer';
-// import { PostCategory } from './component/PostCategory';
 import { RightSideContainer } from './component/rightside/RightSideContainer';
-
-// interface ContentContainerProps {
-//   isOpenList: boolean;
-//   themeMode: ThemeMode;
-// }
 
 const Container = styled.div({
   display: 'flex',
   width: '100%',
-  maxWidth: '1480px',
+  maxWidth: '1000px',
   margin: '0 auto',
   justifyContent: 'stretch',
   alignItems: 'flex-start',
@@ -58,33 +52,6 @@ const Comment = styled.section({
   width: '100%'
 });
 
-const Rotate = keyframes({
-  from: {
-    transform: 'rotate(25deg)'
-  },
-  to: {
-    transform: 'rotate(-25deg)'
-  }
-});
-
-const PostCategoryMobileButton = styled.button<{ holdingButton: boolean }>(
-  (props) => ({
-    display: 'flex',
-    position: 'fixed',
-    bottom: '1.5rem',
-    right: '1.5rem',
-    padding: '.85rem',
-    borderRadius: '50%',
-    backgroundColor: props.theme.secondaryContentBackground,
-    boxShadow: `0 6px 3px -3px ${props.theme.shadowColor}`,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }),
-  css<{ holdingButton: boolean }>`
-    animation: ${(props) => props.holdingButton && Rotate} 0.2s infinite alternate linear;
-  `
-);
-
 interface ServerSideProps {
   categoryId: number;
   post: Post;
@@ -97,97 +64,37 @@ interface ServerSideProps {
 interface Props extends AppCommonProps, ServerSideProps {}
 
 export default function PostId(props: Props) {
-  // const themeMode: ThemeMode = useSelector<RootState, any>((state) => state.common.theme);
-  const post: Post = props.post;
-  const comments: Comments = props.comment;
-  const titles: [{ title: string; _id: number }] = props.sameCategoryTitles;
-  const category: { title: string } = props.category;
-  const profile: ProfileType = props.profile;
-  const categoryId: number = props.categoryId;
-  let touchStartX: number;
-  let touchStartY: number;
-  let touchEndX: number;
-  let touchEndY: number;
-
-  const width = useWidth();
-  const buttonRef = useRef<HTMLButtonElement>(null);
   const commentRef = useRef<HTMLElement>(null);
-  const [showPostCategory, setShowPostCategory] = useState(false);
-  const [isHoldingButton, setIsHoldingButton] = useState(false);
 
   useEffect(() => {
     window.scroll(0, 0);
   }, []);
-
-  function handleTouchStart(event: React.TouchEvent<HTMLButtonElement>) {
-    event.preventDefault();
-    // setTimeout(() => setIsHoldingButton(true), 1000);
-  }
-
-  function handleTouchMove(event: React.TouchEvent<HTMLButtonElement>) {
-    const newX = event.touches[0].clientX;
-    const newY = event.touches[0].clientY;
-    if (isHoldingButton && buttonRef.current) {
-      buttonRef.current.style.left = `${newX}px`;
-      buttonRef.current.style.top = `${newY}px`;
-    }
-  }
-
-  function handleTouchEnd(event: React.TouchEvent<HTMLButtonElement>) {
-    // event.preventDefault();
-    if (isHoldingButton) {
-      setIsHoldingButton(false);
-    }
-  }
 
   function scrollToComment() {
     window.scrollTo(0, commentRef.current?.offsetTop!);
   }
 
   return (
-    <Container
-    // onTouchStart={(event: React.TouchEvent) => handleTouchStart(event)}
-    // onTouchEnd={(event: React.TouchEvent) => handleTouchEnd(event)}
-    >
-      {/* {width > 767 ? (
-        <PostCategory category={category} titles={titles} currentPostId={post._id} isLogin={props.app.isLogin} />
-      ) : (
-        <FocusWrapper visible={showPostCategory} onClickOutside={() => setShowPostCategory(false)}>
-          <PostCategory category={category} titles={titles} currentPostId={post._id} isLogin={props.app.isLogin} />
-        </FocusWrapper>
-      )} */}
+    <Container>
       <ContentContainer>
         <ArticleContainer
-          title={post.title}
-          profile={profile}
-          createdAt={post.createdAt}
-          article={post.article}
+          title={props.post.title}
+          profile={props.profile}
+          createdAt={props.post.createdAt}
+          article={props.post.article}
           isLogin={props.app.isLogin}
         />
         <Comment ref={commentRef}>
           <CommentContainer
-            comments={comments}
+            comments={props.comment}
             isLogin={props.app.isLogin}
-            author={profile.name!}
-            categoryId={categoryId}
-            postId={post._id}
+            author={props.profile.name!}
+            categoryId={props.categoryId}
+            postId={props.post._id}
           />
         </Comment>
       </ContentContainer>
-      <RightSideContainer commentsCount={comments.count} scrollToComment={scrollToComment} />
-      {width <= 767 && !showPostCategory && (
-        <PostCategoryMobileButton
-          holdingButton={isHoldingButton}
-          onClick={() => setShowPostCategory(true)}
-          onTouchStart={(e) => {
-            handleTouchStart(e);
-          }}
-          onTouchMove={(e) => handleTouchMove(e)}
-          onTouchEnd={(e) => handleTouchEnd(e)}
-        >
-          <FontAwesomeIcon icon={faSwatchbook} />
-        </PostCategoryMobileButton>
-      )}
+      <RightSideContainer commentsCount={props.comment.count} scrollToComment={scrollToComment} />
     </Container>
   );
 }
