a68d1ba6829a536ffdacef9959168df43c2a9d2e
Daniel An <daniel@momoproject.co>
2021-08-24 22:30:18 +0900

    Bug fix: delete dynamic for markdown viewer

--- client/src/components/common/md-viewer/Viewer.tsx
+++ client/src/components/common/md-viewer/Viewer.tsx
@@ -1,11 +1,9 @@
-// import { Viewer } from '@toast-ui/react-editor';
-// import dynamic from 'next/dynamic';
-import remarkGfm from 'remark-gfm';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import ReactMarkdown from 'react-markdown';
-import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import ReactMarkdown from "react-markdown";
+import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
-import styles from 'src/styles/markdown-styles.module.css';
+import styles from "src/styles/markdown-styles.module.css";
 
 interface Props {
   content: string;
@@ -41,19 +39,19 @@ export function MDViewer(props: Props) {
           return <h6 id={text}>{text}</h6>;
         },
         img({ src }) {
-          return <img src={src} style={{ width: '100%' }} />;
+          return <img src={src} style={{ width: "100%" }} />;
         },
         code({ className, children }) {
           // Removing "language-" because React-Markdown already added "language-"
-          const language = className?.replace('language-', '');
+          const language = className?.replace("language-", "");
           return (
             <SyntaxHighlighter style={materialDark} language={language}>
               {children[0]}
             </SyntaxHighlighter>
           );
-        }
+        },
       }}
-      className={styles['markdown-body']}
+      className={styles["markdown-body"]}
     >
       {props.content}
     </ReactMarkdown>
