d5ac9bbae561455c701e984032f1c20635284510
JeongHeon Woo <email@wookingwoo.com>
2021-08-28 08:54:37 +0900

    Delete unusable codes

--- client/src/pages/admin/posts.tsx
+++ client/src/pages/admin/posts.tsx
@@ -1,89 +1,22 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { GetServerSideProps } from 'next';
-import Link from 'next/link';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import { useMutation, useApolloClient } from '@apollo/client';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-
 import { GET_LATEST_POSTS, GetLastestPostsQueryType, GetLatestPostsVars, PostDetailDataType } from 'src/query/post';
 import { trans, Lang } from 'src/resources/languages';
 import { initializeApollo } from 'src/lib/apollo';
-import { BorderBox, CircleRippleWrapper } from 'src/components';
 import { appCommponProps, AppCommonProps } from 'src/pages/_app';
 import { AdminPageLayout, PageTitle } from 'src/components/pages/admin';
 import { FormatUnifier } from 'src/utils';
-
 import { DeletePostQueryType, DeletePostVars, DELETE_POST } from 'src/query/post';
 import { IsAuthQueryType, IS_AUTH } from 'src/query/user';
 import { DeletePostAllCommentLogQueryType, DeletePostAllCommentLogVars, DELETE_POST_ALL_COMMENT_LOG } from 'src/query/comment-log';
 
 const Container = styled.div({
   width: '100%'
-});
-
-const PostContainer = styled.div({
-  width: '100%'
-});
-
-const Wrapper = styled.div({
-  position: 'relative',
-  flex: 1
-});
-
-const DeleteButtonWrapper = styled.div({
-  position: 'absolute',
-  top: '.2rem',
-  right: '1rem',
-  zIndex: 1,
-
-  '&:hover': {
-    transform: 'translateY(-1px)',
-    boxShadow: '1px 8px 5px -3px gray'
-  }
-});
-
-const Content = styled.div({
-  padding: '.8rem',
-  width: '100%',
-  height: '7rem'
-});
-
-const PreviewTextWrapper = styled.div({
-  width: '100%',
-  height: '100%'
-});
-
-const PreviewTitle = styled.span({
-  flexShrink: 0,
-  height: '2rem',
-  width: '100%',
-  fontSize: '1.4rem',
-  fontWeight: 'bold',
-  textAlign: 'left',
-  wordBreak: 'break-all',
-  overflow: 'hidden',
-  display: '-webkit-box',
-  WebkitLineClamp: 1,
-  WebkitBoxOrient: 'vertical',
-  '@media screen and (max-width: 1380px)': {
-    wordBreak: 'break-all'
-  }
-});
-
-const PreviewContent = styled.span({
-  flexShrink: 0,
-  width: '100%',
-  margin: '.8rem 0 0',
-  wordBreak: 'keep-all',
-  textAlign: 'left',
-  display: '-webkit-box',
-  WebkitLineClamp: 1,
-  WebkitBoxOrient: 'vertical',
-  overflow: 'hidden'
 });
 
 const BoardTD = styled.td({
@@ -216,32 +149,6 @@ export default function PostProps(props: Props) {
             <tbody>
               {props.posts.map((post) => {
                 return (
-                  // <Link key={post.title + post._id} href={`/post/${post._id}`} passHref>
-                  //   <PostContainer>
-                  //     <BorderBox isHoverEffect={true} styles={{ width: '100%', margin: '.8rem 0' }}>
-                  //       <Wrapper>
-                  //         <DeleteButtonWrapper>
-                  //           <CircleRippleWrapper
-                  //             onClick={(event) => {
-                  //               event.stopPropagation();
-                  //               // setDeletedPost({ isModalOpen: true, index });
-                  //               alert('준비중');
-                  //             }}
-                  //           >
-                  //             <FontAwesomeIcon icon={faTrash} style={{ fontSize: '1.25rem' }} />
-                  //           </CircleRippleWrapper>
-                  //         </DeleteButtonWrapper>
-                  //         <Content>
-                  //           <PreviewTextWrapper>
-                  //             <PreviewTitle>{post.title}</PreviewTitle>
-                  //             <PreviewContent>{post.article}</PreviewContent>
-                  //           </PreviewTextWrapper>
-                  //         </Content>
-                  //       </Wrapper>
-                  //     </BorderBox>
-                  //   </PostContainer>
-                  // </Link>
-
                   <BoardTR key={post.title + post._id}>
                     <DynamicDeleteModal visible={isModalOpen} onDelete={handleDeleteButtonClick} onCancel={handleCancelButtonClick} />
 
