ad11506b9ee5ba0927a24fdba89f8b83dc27cb93
Daniel An <daniel@momoproject.co>
2021-09-25 21:48:44 +0900

    Feature: Pagination button added

--- client/src/components/pages/main/post/PostContainer.tsx
+++ client/src/components/pages/main/post/PostContainer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
 import { PostDetailDataType } from 'src/query/post';
 
@@ -10,17 +11,35 @@ const Container = styled.div({
   padding: '0 1rem'
 });
 
+const FlexWrapper = styled.div({
+  display: 'flex',
+  width: '100%',
+  justifyContent: 'center'
+});
+
+const SeeMoreButton = styled.button({
+  padding: '.5rem',
+  borderRadius: '.5rem'
+});
+
 interface Props {
   posts: PostDetailDataType[];
+  totalPosts: number;
 }
 
 export function PostContainer(props: Props) {
+  const router = useRouter();
+
   return (
     <Container>
       {props.posts.map((post) => (
         <PostItem key={post.article + post._id} post={post} />
       ))}
-      {props.posts.length === 10 && <div>더보기</div>}
+      {props.posts.length === 10 && (
+        <FlexWrapper>
+          <SeeMoreButton onClick={() => router.push({ pathname: '/category/0', query: { page: 2 } })}>더보기</SeeMoreButton>
+        </FlexWrapper>
+      )}
     </Container>
   );
 }
