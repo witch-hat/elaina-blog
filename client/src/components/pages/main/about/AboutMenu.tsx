4c9c229eabbcd46cdac948a3fdfb8becd6f7538f
Daniel An <daniel@momoproject.co>
2021-09-25 21:46:29 +0900

    About editor changed

--- client/src/components/pages/main/about/AboutMenu.tsx
+++ client/src/components/pages/main/about/AboutMenu.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useRouter } from 'next/router';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
-
-import { DropDownMenu } from 'src/components';
-import { trans, Lang } from 'src/resources/languages';
 
 import { MemoizedAboutInfo } from './AboutInfo';
 
@@ -16,16 +10,6 @@ const FlexWrapper = styled.div({
   alignItems: 'center'
 });
 
-const Button = styled.button((props) => ({
-  padding: '.5rem',
-  borderRadius: '.5rem',
-  textAlign: 'center',
-  wordBreak: 'keep-all',
-  '&:hover': {
-    backgroundColor: props.theme.hoverBackground
-  }
-}));
-
 interface Props {
   name: string;
   updatedAt: number;
@@ -33,19 +17,9 @@ interface Props {
 }
 
 export function AboutMenu(props: Props) {
-  const router = useRouter();
-
   return (
     <FlexWrapper>
       <MemoizedAboutInfo name={props.name} updatedAt={props.updatedAt} />
-      {props.isLogin && (
-        <DropDownMenu
-          mainButton={<FontAwesomeIcon icon={faEllipsisV} />}
-          dropMenu={
-            <Button onClick={() => router.replace(router.asPath + '&edit=true', undefined, { shallow: true })}>{trans(Lang.Edit)}</Button>
-          }
-        />
-      )}
     </FlexWrapper>
   );
 }
